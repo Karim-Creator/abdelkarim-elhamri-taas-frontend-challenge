afc9ce28ff806a55ed42f7d94ddeb29b3bc0b56c
karim El Hamri <develkarim@gmail.com>
2023-04-08 00:28:11 +0000

    Coding Challenge Start

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// tailwind.config.js
+module.exports = {
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0969DA',
+                secondary: '#1F883D',
+                dark: '#24292F',
+                light: '#EFF1F3',
+                grey: '#F6F8FA',
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
